4dc9e9e17c591dc76fef1050eec53fa1fae4ed2c
aloktech <meher.ranjan@gmail.com>
2016-11-11 16:55:12 +0530

    Add two Shiro class

--- SampleShiro/src/main/java/com/imos/sample/SampleShiroOne.java
+++ SampleShiro/src/main/java/com/imos/sample/SampleShiroOne.java
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package com.imos.sample;
 
 import org.apache.shiro.SecurityUtils;
@@ -12,21 +13,29 @@ import org.apache.shiro.authc.IncorrectCredentialsException;
 import org.apache.shiro.authc.LockedAccountException;
 import org.apache.shiro.authc.UnknownAccountException;
 import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
 import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
+import org.apache.shiro.mgt.SecurityManager;
 
 /**
  *
  * @author alok
  */
-public class SampleShiro {
+public class SampleShiroOne {
 
     public static void main(String[] args) {
         System.out.println("s");
 
+        Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro.ini");
+        SecurityManager securityManager = factory.getInstance();
+
+        SecurityUtils.setSecurityManager(securityManager);
+
         //Example using most common scenario: 
         //String username and password.  Acquire in 
         //system-specific manner (HTTP request, GUI, etc)
-        UsernamePasswordToken token = new UsernamePasswordToken("", "");
+        UsernamePasswordToken token = new UsernamePasswordToken("lonestarr", "vespa");
 
         //”Remember Me” built-in, just do this:         
         token.setRememberMe(true);
@@ -47,8 +56,6 @@ public class SampleShiro {
         } catch (LockedAccountException lae) {
         } catch (ExcessiveAttemptsException eae) {
         } catch (AuthenticationException ae) {
-            //unexpected error?
         }
-        //No problems, show authenticated view…
     }
 }
---
+++ SampleShiro/src/main/java/com/imos/sample/SampleShiroTwo.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.imos.sample;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.ExcessiveAttemptsException;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.LockedAccountException;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
+
+/**
+ *
+ * @author alok
+ */
+public class SampleShiroTwo {
+
+    public static void main(String[] args) {
+        System.out.println("s");
+        
+        Factory<org.apache.shiro.mgt.SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro.ini");
+        org.apache.shiro.mgt.SecurityManager securityManager = factory.getInstance();
+
+        SecurityUtils.setSecurityManager(securityManager);
+        
+        //Example using most common scenario: 
+        //String username and password.  Acquire in 
+        //system-specific manner (HTTP request, GUI, etc)
+        UsernamePasswordToken token = new UsernamePasswordToken("lonestarr", "vespa");
+//        UsernamePasswordToken token = new UsernamePasswordToken("", "");
+
+        //”Remember Me” built-in, just do this:         
+        token.setRememberMe(true);
+
+        //With most of Shiro, you'll always want to make sure you're working with the currently 
+        //executing user, referred to as the subject 
+        Subject currentUser = SecurityUtils.getSubject();
+
+        //Authenticate the subject by passing 
+        //the user name and password token 
+        //into the login method 
+        currentUser.login(token);
+
+        try {
+            currentUser.login(token);
+        } catch (UnknownAccountException uae) {
+        } catch (IncorrectCredentialsException ice) {
+        } catch (LockedAccountException lae) {
+        } catch (ExcessiveAttemptsException eae) {
+        } catch (AuthenticationException ae) {
+        }
+    }
+}
